89784b90fb7a994c39cdf082860024c28cfecc87
VinitKarlekar <vinitkarlekar84474@gmail.com>
2025-03-09 21:12:58 +0530

    migration

--- CRUDX/settings.py
+++ CRUDX/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*^vy3z*od$e*c$il^s#6&m^*!ea&5-)3j*ukgyh^r7^p-4!c8t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.onrender.com','127.0.0.1']
 
